8e8e6508ada4e627b7ec5f2e17cf2152aa283241
mrzwk-b <bmerkey@gmail.com>
2025-03-04 10:47:47 -0500

    added loop to check if guesses are options

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:wordle/distribution.dart';
 import 'package:wordle/evaluators/positionless_evaluator.dart';
 import 'package:wordle/optimizer.dart';
@@ -8,5 +10,9 @@ void main() async {
   final Set<String> past = await getPastAnswers();
   final Set<String> options = possible.difference(past);
   final Map<String, LetterDistribution> distribution = getDistribution(options);
-  print(optimal(options, PositionlessEvaluator(distribution)));
+  print("opener: ${optimal(options, PositionlessEvaluator(distribution))}");
+  while (true) {
+    print("enter a word to see if it's a possible answer today:");
+    print(options.contains(stdin.readLineSync()) ? "yes" : "no");
+  }
 }
