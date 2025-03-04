83ad5296b5a1ab1d92e316fd8e800a072324bc9e
mrzwk-b <bmerkey@gmail.com>
2025-03-04 16:46:38 -0500

    added option to include today's answer

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,38 @@
 import 'dart:io';
 
+import 'package:args/args.dart';
 import 'package:wordle/distribution.dart';
 import 'package:wordle/evaluators/contextless_positional_evaluator.dart';
 import 'package:wordle/evaluators/positionless_evaluator.dart';
 import 'package:wordle/optimizer.dart';
 import 'package:wordle/scrape.dart';
 
-void main() async {
+void main(List<String> argStrs) async {
+  ArgResults args = (ArgParser()..addOption("today", abbr: 't')).parse(argStrs);
+
   late final Set<String> possible;
   late final Set<String> past;
   for (Future assignment in [
     getPossibleAnswers().then((result) {possible = result;}),
     getPastAnswers().then((result) {past = result;}),
   ]) await assignment;
+
+  if (args.option("today") != null) {
+    past.add(args.option("today")!);
+  }
+
   final Set<String> options = possible.difference(past);
   final Map<String, LetterDistribution> distribution = getDistribution(options);
-  print("opener: ${optimal(options, ContextlessPositionalEvaluator(distribution))}");
+  print(
+    "opener (Positionless): "
+    "${optimal(options, PositionlessEvaluator(distribution))}"
+  );
+  print(
+    "opener (ContextlessPositional): "
+    "${optimal(options, ContextlessPositionalEvaluator(distribution))}"
+  );
   while (true) {
-    print("enter a word to see if it's a possible answer today:");
+    print("enter a word to see if it's a possible answer:");
     print(options.contains(stdin.readLineSync()) ? "yes" : "no");
   }
 }
