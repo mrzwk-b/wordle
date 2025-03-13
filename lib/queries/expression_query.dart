1d7c98bd0415361791342c0cdb5e4a263d5e6621
mrzwk-b <bmerkey@gmail.com>
2025-03-13 17:16:44 -0400

    implemented variable expression queries

--- lib/queries/expression_query.dart
+++ lib/queries/expression_query.dart
@@ -17,6 +17,81 @@ bool isLetterMatch(final String letter, final String pattern) =>
   }
 ;
 
+bool isMatch(final String word, final String pattern) {
+  // loop through the whole word
+  for (int wordStart = 0; wordStart < 5; wordStart++) {
+    // start trying to match the pattern from where we are in the word
+    int wordIndex = wordStart;
+    bool matching = true;
+    for (int patternIndex = 0; patternIndex < pattern.length; patternIndex++) {
+      if (pattern[patternIndex] == '#') {
+        matching = wordIndex == 0 || wordIndex == 5;
+      }
+      else if (wordIndex < 5) {
+        switch (pattern[patternIndex]) {
+          case '@':
+            if (!vowels.contains(word[wordIndex])) {
+              matching = false;
+            }
+            else {
+              wordIndex++;
+            }
+          ;
+          case r'$':
+            if (!consonants.contains(word[wordIndex])) {
+              matching = false;
+            }
+            else {
+              wordIndex++;
+            }
+          ;
+          case '?':
+            throw QueryException('"?" cannot occur in fixed expression query');
+          case '_':
+            if (patternIndex + 1 < pattern.length && pattern[patternIndex+1] == '*') {
+              while (wordIndex < 5 && isLetterMatch(word[wordIndex], pattern[patternIndex])) {
+                wordIndex++;
+              }
+              patternIndex++;
+            }
+            else if (pattern[patternIndex] == '+') {
+              if (wordIndex >= 5) {
+                return false;
+              }
+              while (wordIndex < 5 && isLetterMatch(word[wordIndex], pattern[patternIndex])) {
+                wordIndex++;
+              }
+              patternIndex++;
+            }
+            else {
+              wordIndex++;
+            }
+          ;
+          default:
+            matching &= word[wordIndex] == pattern[patternIndex];
+            wordIndex++;
+          ;
+        }
+      }
+      else {matching = false;}
+      if (!matching) {break;}
+    }
+
+    if (matching) return true;
+  }
+  return false;
+}
+
+List<String> getMatchingLetters(final String word, final String pattern) {
+  List<String> matchingLetters = [];
+  for (String letter in alphabet) {
+    if (isMatch(word, pattern.replaceAll('?', letter))) {
+      matchingLetters.add(letter);
+    }
+  }
+  return matchingLetters;
+}
+
 class ExpressionQuery extends Query {  
   String pattern;
 
@@ -31,102 +106,37 @@ class ExpressionQuery extends Query {
     }
   }
 
-  bool isMatch(final String word) {
-    // loop through the whole word
-    for (int wordStart = 0; wordStart < 5; wordStart++) {
-      // start trying to match the pattern from where we are in the word
-      int wordIndex = wordStart;
-      bool matching = true;
-      for (int patternIndex = 0; patternIndex < pattern.length; patternIndex++) {
-        if (pattern[patternIndex] == '#') {
-          matching = wordIndex == 0 || wordIndex == 5;
-        }
-        else if (wordIndex < 5) {
-          switch (pattern[patternIndex]) {
-            case '@':
-              if (!vowels.contains(word[wordIndex])) {
-                matching = false;
-              }
-              else {
-                wordIndex++;
-              }
-            ;
-            case r'$':
-              if (!consonants.contains(word[wordIndex])) {
-                matching = false;
-              }
-              else {
-                wordIndex++;
-              }
-            ;
-            case '?':
-              throw QueryException('"?" cannot occur in fixed expression query');
-            case '_':
-              if (patternIndex + 1 < pattern.length && pattern[patternIndex+1] == '*') {
-                while (wordIndex < 5 && isLetterMatch(word[wordIndex], pattern[patternIndex])) {
-                  wordIndex++;
-                }
-                patternIndex++;
-              }
-              else if (pattern[patternIndex] == '+') {
-                if (wordIndex >= 5) {
-                  return false;
-                }
-                while (wordIndex < 5 && isLetterMatch(word[wordIndex], pattern[patternIndex])) {
-                  wordIndex++;
-                }
-                patternIndex++;
-              }
-              else {
-                wordIndex++;
-              }
-            ;
-            default:
-              matching &= word[wordIndex] == pattern[patternIndex];
-              wordIndex++;
-            ;
-          }
-        }
-        else {matching = false;}
-        if (!matching) {break;}
-      }
-
-      if (matching) return true;
-    }
-    return false;
-  }
-
-  List<String> getMatchingLetters(final String word) {
-    return [""]; // TODO
-  }
-
   @override
   String execute() {
     if (pattern.contains('?')) {
-      throw UnimplementedError("variable ExpressionQueries are not implemented yet"); // TODO
-      // int unmatchedWordCount = 0;
-      // final Iterable<List<String>> letterMatchesByWord = options.map(getMatchingLetters);
-      // final Map<String, int> letterMatchCounts = Map.fromIterable(alphabet,
-      //   key: (letter) => letter,
-      //   value: (letter) => 0,
-      // );
-      // for (final List<String> matchedLetters in letterMatchesByWord) {
-      //   if (matchedLetters.length == 0) {
-      //     unmatchedWordCount += 1;
-      //   }
-      //   for (final String letter in matchedLetters) {
-      //     letterMatchCounts.update(letter, (count) => count + 1);
-      //   }
-      // }
-      // return [
-      //   for (final String letter in rank(letterMatchCounts)) 
-      //     "$letter: ${letterMatchCounts[letter]}"
-      //   ,
-      //   "words that matched with no letters: $unmatchedWordCount"
-      // ].join('\n');
+      int unmatchedWordCount = 0;
+      final Iterable<List<String>> letterMatchesByWord = options.map((word) => getMatchingLetters(word, pattern));
+      final Map<String, int> letterMatchCounts = Map.fromIterable(alphabet,
+        key: (letter) => letter,
+        value: (letter) => 0,
+      );
+      for (final List<String> matchedLetters in letterMatchesByWord) {
+        if (matchedLetters.length == 0) {
+          unmatchedWordCount += 1;
+        }
+        for (final String letter in matchedLetters) {
+          letterMatchCounts.update(letter, (count) => count + 1);
+        }
+      }
+      return [
+        for (final String letter in rank(letterMatchCounts))
+          if (letterMatchCounts[letter]! != 0) "$letter: ${letterMatchCounts[letter]}"
+        ,
+        "letters that matched with no words: "
+          "${letterMatchCounts.keys.where((letter) => letterMatchCounts[letter]! == 0)}"
+        ,
+        "words that matched with no letters: $unmatchedWordCount"
+      ].join('\n');
     }
     else {
-      List<String> results = [for (final String word in options.where(isMatch)) word];
+      List<String> results = [
+        for (final String word in options.where((word) => isMatch(word, pattern))) word
+      ];
       return 
         "${results.join('\n')}\n\n"
         "total: ${results.length}"
